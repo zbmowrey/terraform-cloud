7d2c003e9fbfdda46d98c1d933a15ce4e7f8024f
Zach Mowrey <zbmowrey@gmail.com>
2021-11-28 10:59:06 -0500

    Add Slack notification to terraform-cloud workspace

--- main.tf
+++ main.tf
@@ -14,5 +14,24 @@ terraform {
 }
 
 provider "tfe" {
-  token = var.access_keys["tf_cloud"]
+  token = "HmjeiIqsEghTNQ.atlasv1.Bm8zDKVxTmO4qdAA3sCAewuuIbFu56VQXD6chgsGijBxhJnHY3jox0fGBXIOH4g7gR4"
+}
+
+resource "tfe_workspace" "terraform-cloud" {
+  name         = "terraform-cloud"
+  organization = tfe_organization.zbmowrey-cloud-admin.name
+  vcs_repo {
+    identifier     = "zbmowrey/terraform-cloud"
+    oauth_token_id = data.tfe_oauth_client.cloud-admin.oauth_token_id
+    branch         = "main"
+  }
+}
+
+resource "tfe_notification_configuration" "terraform-cloud" {
+  destination_type = "slack"
+  enabled          = true
+  url              = var.terraform_slack_url
+  name             = "Terraform Cloud"
+  workspace_id     = tfe_workspace.terraform-cloud.id
+  triggers = ["run:created", "run:needs_attention", "run:completed", "run:errored"]
 }
