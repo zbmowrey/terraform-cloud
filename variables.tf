64b068497b4b0c7f2d59eccb39d4e86dc376c994
Zach Mowrey <zbmowrey@gmail.com>
2021-11-28 13:29:17 -0500

    Begin the process of unifying under one stack

--- variables.tf
+++ variables.tf
@@ -4,3 +4,15 @@ variable oauth_clients {}
 variable aws_root_key {}
 variable aws_root_secret {}
 variable terraform_slack_url {}
+
+# Variables for version-control.tf
+variable "aws_key_main" {}
+variable "aws_secret_main" {}
+variable "aws_key_staging" {}
+variable "aws_secret_staging" {}
+variable "aws_key_develop" {}
+variable "aws_secret_develop" {}
+variable "github_token" {}
+variable "terraform_token" {}
+variable "serverless_token" {}
+variable "cf_distributions" {}
